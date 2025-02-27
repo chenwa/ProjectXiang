761da4b824a655a54433c23ffaaa6ba501b1b7f3
Warren Chen <warren@iwarren.com>
2025-02-27 14:06:56 -0800

    added logging

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from fastapi import FastAPI
+import logging
+
+
+def setup_logger(log_file):
+    logger = logging.getLogger("MyLogger")
+    logger.setLevel(logging.DEBUG)
+    # Create a file handler to write logs to a file
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+    # Create a console handler to output logs to the console
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # Create a formatter to specify the log message format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # Add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
 
 app = FastAPI()
+logger = setup_logger("logs.log")
 
 @app.get("/")
 def read_root():
-    print("hello")
+
+    logger.info("/ reached")
     return {"message": "Hello, World!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    print("hi")
+    logger.info("/items/{" + str(item_id) + "}")
     return {"item_id": item_id, "q": q}
 
